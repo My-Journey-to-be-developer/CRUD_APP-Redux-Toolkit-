9dd0b36080ec1422a5b143225f9437e3643b2e75
My-Journey-to-be-developer <kdpatel763@gmail.com>
2024-04-04 15:04:25 -0400

    store setup

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit"
+import { userDetail } from "./features/UserDetailSlice"
+
+export const store = configureStore({
+    reducer:{
+        app: userDetail,
+    },
+});
+export default store;
